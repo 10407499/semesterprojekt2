266505a51a3c8b3ddaadc6b9a4d73d150d133734
Christopher <93257325+CsnUcn@users.noreply.github.com>
2022-05-12 10:09:48 +0200

    doc creator updates

--- src/gui/DocumentCreator.java
+++ src/gui/DocumentCreator.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import com.spire.doc.Document;
 import com.spire.doc.DocumentViewType;
 import com.spire.doc.FileFormat;
+import com.spire.doc.LineSpacingRule;
 import com.spire.doc.Section;
 import com.spire.doc.documents.BuiltinStyle;
 import com.spire.doc.documents.HorizontalAlignment;
@@ -17,10 +18,11 @@ import com.spire.doc.fields.DocPicture;
 import com.spire.doc.formatting.ParagraphFormat;
 
 import model.Order;
+import model.OrderLine;
 
 public class DocumentCreator {
 	
-	public DocumentCreator() {
+	public DocumentCreator(Order order) {
 		
 		String templateA4 = "templateA4.docx";
 		
@@ -48,21 +50,22 @@ public class DocumentCreator {
 		
 		//Paragraph for header
 		Paragraph orderInfo = header.addParagraph();
+		orderInfo.getFormat().setAfterSpacing(1);
 
 		//Body for header
         orderInfo.appendText("Bestilling: \n"
-        		+ "Dato: \n"
-        		+ "Mail: \n"
-        		+ "Adr: \n"
-        		+ "Antal: \n"
-        		+ "Gæster ankommer kl: \n"
-        		+ "Spisetid: Afgang fra Jebjr.: \n"
+        		+ "Dato: " + order.getFulfillmentDate() + "\n"
+        		+ "Mail: " + order.getCustomer().getEmail() +"\n"
+        		+ "Adr: " + order.getCustomer().getStreet() + " " + order.getCustomer().getHouseNo() + ", " + order.getCustomer().getZipCode() + " " + order.getCustomer().getCity() + "\n"
+        		+ "Antal: " + order.getCoverAmount() + "\n"
+        		+ "Gæster ankommer kl: " +  "\n" //FIXME tilføjes spisetidspunkt til order 
+        		+ "Spisetid: Afgang fra Jebjr.: \n" //FIXME
         		+ "Ankomst: I vil få besked i dagene før jeres fest. \n"
         		+ "");
         
 		//Paragraph for general information
 		Paragraph generalInfo = header.addParagraph();
-
+		generalInfo.getFormat().setAfterSpacing(1);
 		//Body for general information
         generalInfo.appendText("Hej\n"
         		+ "Tak for snakken og for din bestilling. forespørgsel. Det vil vi rigtig gerne lave til dig.\n"
@@ -75,19 +78,37 @@ public class DocumentCreator {
         		" og se vores menuer.\n"
         		+ "Ved forespørgsel er det vigtig vi får besked om I ønsker at benytte vores tilbud.\n"
         		+ "\n"
-        		+ "Lidt generalt: \n"
+        		+ "Lidt generelt: \n"
         		+ "Her er også lidt med vigtige oplysninger og vejledning om at modtage menuen fra os. \n"
         		+ "Her vil du kunne finde de retter I har bestilt. En vejledning kan ses her: \n");
         generalInfo.appendHyperlink("www.spændendemad.dk/vejledning ","www.spændendemad.dk/vejledning", HyperlinkType.Web_Link);
         generalInfo.appendText("\n"
         		+ "\n"
-        		+ "Hvis I har ændringer til antal couv., vil jeg gerne I mailer det aktuelle antal 10 dage før jeres fest, \n"
-        		+ "da vi ønsker at have dokumenter klar til at købe ind mandag morgen. Spisetid må også være oplyst \n"
-        		+ "her \n"
+        		+ "Hvis I har ændringer til antal couv., vil jeg gerne I mailer det aktuelle antal 10 dage før jeres \n"
+        		+ "fest, da vi ønsker at have dokumenter klar til at købe ind mandag morgen. \n"
+        		+ "Spisetid må også være oplyst her \n"
         		+ "Ønsker du maden leveret? Ved levering må du oplyse leverings adresse. \n"
         		+ "\n"
         		);
         
+       generalInfo.appendText("Vedr. betaling: Hvis ikke andet er aftalt, betales der ved levering/afhentning, dette kan gøres med Swipp, Mobilpay eller kontant, i vil modtage en mail med fakturaen senest 2 dage før festen \n"
+    		   + "\n"
+    		   + "Vi vil gerne I aflevere fade mandag efter jeres middag. Her holder vi åbent i køkkenet for at modtage udstyr fra kl. 16.00 til 18.00. \n"
+    		   + "\n"
+    		   + "Hvis I ønsker det, så kan jeg sende en´ eller to med i køkkenet. Serveringshjælp kan vi også henvise til jer \n"
+    		   + "\n"
+    		   + "Du må endelig henvende, hvis du har spørgsmål. dig \n"
+    		   + "                                           De bedste hilsner fra Sanne."
+    		   + "\n \n \n"
+    		   );
+       
+       Paragraph menu = header.addParagraph();
+       menu.appendText("*Menu");
+       for(OrderLine ol : order.getOrderLines()) {
+    	   menu.appendText(ol.getProduct().getDescription());
+       }
+       
+       
         //	Style header text
         ParagraphStyle styleHeader = new ParagraphStyle(doc);
         styleHeader.setName("Header");
@@ -105,7 +126,16 @@ public class DocumentCreator {
         doc.getStyles().add(styleInformation);
         generalInfo.applyStyle("Information");
         
-		doc.saveToFile("outputWordOrder.docx", FileFormat.Docx);
+        ParagraphStyle menuInformation = new ParagraphStyle(doc);
+        menuInformation.setName("menuInfo");
+        menuInformation.getCharacterFormat().setFontName("Times New Roman");
+        menuInformation.getCharacterFormat().setFontSize(14);
+        menuInformation.getCharacterFormat().setItalic(true);
+        doc.getStyles().add(menuInformation);
+        menu.applyStyle("menuInfo");
+        
+        String docname = order.getFulfillmentDate() + " " + order.getCustomer().getfName() + " " + order.getCustomer().getlName() + ".docx";
+		doc.saveToFile(docname, FileFormat.Docx);
 		System.out.println("called");
 		
 	}
